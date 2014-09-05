882071237d441c9b3092e28d6f39575992e66388
Biter Bilen <biter@stanford.edu>
2014-09-05 15:52:31 -0700

    clone function expanded for >1 library users

--- bin/bb_make_project_template.sh
+++ bin/bb_make_project_template.sh
@@ -25,7 +25,7 @@ set -o nounset                              # Treat unset variables as an error
 # 2 TODO config clone and push user restrictions
 # 3 TODO from the above document; is it possible to add an http git interface w lighttpd webrick
 
-GIT_HOSTNAME=sherlock.stanford.edu
+#GIT_HOSTNAME=sherlock.stanford.edu
 GIT_LOCAL_DIR=$PI_SCRATCH/Projects #or $SCRATCH for own projects
 GIT_BARE_DIR=$PI_HOME/Projects.git
 
@@ -39,32 +39,39 @@ function_configGitUser() {
 	#git instaweb --httpd=webrick --stop
 } 
 
+# Enforces project directory structure
 function_cloneGitProject() {
 	project_tag=$1
-	project_user2=${2:-$USER}
+	project_user=$2
+	project_owner=$3
+	clone_project_tag=${4:-$project_tag}
 
-	project_name=`bb_getProjectName $project_tag $project_user2`
-
+	project_name=`bb_getProjectName $project_tag $project_user $project_owner`
 	bare_dir=$GIT_BARE_DIR/$project_name.git
+	clone_project_name=`bb_getProjectName $clone_project_tag $project_user`
+	clone_dir=$GIT_LOCAL_DIR/$clone_project_name
+
 #	git clone $USER@$GIT_HOSTNAME:$bare_dir $project_name
-	git clone file:///$bare_dir $project_name
+	echo git clone file:///$bare_dir $clone_dir
+	git clone file:///$bare_dir $clone_dir
 
 }
 
 #TODO function howto write from plugin
 bb_getProjectName() {
 	project_tag=$1
-	project_user2=$2
+	project_user=$2
+	project_owner=${3:-$USER}
 	
-	echo ${USER}_${project_user2}_${project_tag}
+	echo ${project_owner}_${project_user}_${project_tag}
 }
 
-
+# make one's own projects
 function_makeGitProject() {
 	project_tag=$1
-	project_user2=${2:-$USER}
+	project_user=${2:-$USER}
 
-	project_name=`bb_getProjectName $project_tag $project_user2`
+	project_name=`bb_getProjectName $project_tag $project_user`
 
 	#bare repository
 	bare_dir=$GIT_BARE_DIR/$project_name.git
@@ -83,6 +90,19 @@ function_makeGitProject() {
 	popd
 }
 
+# Delete one's own project
+function_deleteProject() {
+	project_tag=$1
+	project_user=${2:-$USER}
+
+	project_name=`bb_getProjectName $project_tag $project_user`
+
+	bare_dir=$GIT_BARE_DIR/$project_name.git
+	local_dir=$GIT_LOCAL_DIR/$project_name
+	rm -rf $bare_dir $local_dir
+
+}
+
 function_setCommonDirPerms() {
 	#permissions of data folder
 	for folder in $GIT_LOCAL_DIR $GIT_BARE_DIR; do
@@ -90,17 +110,6 @@ function_setCommonDirPerms() {
 		chmod g+s $folder
 		chmod o+t $folder
 	done
-
 }
 
-function_deleteProject() {
-	project_tag=$1
-	project_user2=${2:-$USER}
 
-	project_name=`bb_getProjectName $project_tag $project_user2`
-
-	bare_dir=$GIT_BARE_DIR/$project_name.git
-	local_dir=$GIT_LOCAL_DIR/$project_name
-	rm -rf $bare_dir $local_dir
-
-}
